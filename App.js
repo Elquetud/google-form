76bb490a3219aac9af70ef9474b25a8dc6787597
Mona Lisa <jesusromeroarmenta@outlook.com>
2022-01-18 23:04:31 -0700

    cambios

--- App.js
+++ App.js
@@ -1,38 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import * as Google from 'expo-google-app-auth';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import * as Google from "expo-google-app-auth";
+
+
 
 export default function App() {
-
   const [accessToken, setAccessToken] = React.useState();
   const [userInfo, setUserInfo] = React.useState();
 
   async function signInWithGoogleAsync() {
     try {
       const result = await Google.logInAsync({
-        androidClientId: "503384578811-untujs68h3jgvf0btrhtrf4nf6mhbu9m.apps.googleusercontent.com",
-        iosClientId: "503384578811-s30c6pq41loqgegk3rcqvtfaei37bcuh.apps.googleusercontent.com",
-        scopes: ["profile", "email"]
+        androidClientId:
+          "503384578811-untujs68h3jgvf0btrhtrf4nf6mhbu9m.apps.googleusercontent.com",
+        iosClientId:
+          "503384578811-s30c6pq41loqgegk3rcqvtfaei37bcuh.apps.googleusercontent.com",
+        scopes: ["profile", "email"],
       });
 
       if (result.type === "success") {
         setAccessToken(result.accessToken);
+        
       } else {
         console.log("Permission denied");
       }
+     
     } catch (e) {
       console.log(e);
     }
+
+    getUserData();
   }
 
   async function getUserData() {
-    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      headers: { Authorization: `Bearer ${accessToken}`}
-    });
+    let userInfoResponse = await fetch(
+      "https://www.googleapis.com/userinfo/v2/me",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
 
-    userInfoResponse.json().then(data => {
+    userInfoResponse.json().then((data) => {
       setUserInfo(data);
+      
     });
   }
 
@@ -41,8 +59,10 @@ export default function App() {
       return (
         <View style={styles.userInfo}>
           <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
-          <Text>Welcome {userInfo.name}</Text>
-          <Text>{userInfo.email}</Text>
+          <Text style={{color: "#FFFFFF"}}>
+            Bienvenido : {userInfo.name}
+          </Text>
+          <Text style={{color: "#FFFFFF"}}>{userInfo.email}</Text>
         </View>
       );
     }
@@ -50,8 +70,34 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      {accessToken ? (
+        <View>
+          <Text style={styles.texthome}>Bienvenido  </Text>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Image
+            style={styles.tinyLogo}
+            source={{uri:'https://canteradigital.mx/cantera2020/wp-content/uploads/2020/05/CanteraLogo.png'}}
+          />
+
+          <Text style={styles.text}>Página de Registro</Text>
+        </View>
+      )}
+      <View style={styles.containerbutton}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={accessToken ? getUserData : signInWithGoogleAsync}
+      >
+        <Text style={styles.buttonText}>
+          
+          {accessToken ? "Detalles de cuenta" : "Iniciar con Google"}
+        </Text>
+      </TouchableOpacity>
+      </View>
+
       {showUserInfo()}
-      <Button title={accessToken ? "Obtener Detalles de cuenta" : "Login"} onPress={accessToken ? getUserData : signInWithGoogleAsync} />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -60,16 +106,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#031835",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerbutton:{
+    flex:0.5
+  },
+  tinyLogo:{
+    width:170,
+    height:50
+  },
+  text: {
+    marginTop:20,
+    fontSize: 28,
+    color: "#FFFFFF"
+  },
+  texthome:{
+    fontSize:19,
+    color: "#FFFFFF"
+  },  
+  button: {
+    alignItems: "center",
+    backgroundColor: "#4286F4",
+    marginTop: 30,
+    padding: 15,
+    borderBottomEndRadius: 4,
+    borderBottomStartRadius: 4,
+    width: 190,
+    height: 55,
+  },
+  buttonText: {
+    color: "#FFFFFF",
   },
   userInfo: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   profilePic: {
     width: 50,
-    height: 50
-  }
+    height: 50,
+  },
+
 });
