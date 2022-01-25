13d7e5c840019d0846e85b87c9e6e72474e8f5ba
Mona Lisa <jesusromeroarmenta@outlook.com>
2022-01-25 15:40:04 -0700

    miapp

--- App.js
+++ App.js
@@ -9,8 +9,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 import * as Google from "expo-google-app-auth";
-
-
+import img from './assets/1589983314924.jpeg';
+import icon from './assets/logog.png';
 
 export default function App() {
   const [accessToken, setAccessToken] = React.useState();
@@ -37,7 +37,7 @@ export default function App() {
       console.log(e);
     }
 
-    getUserData();
+    
   }
 
   async function getUserData() {
@@ -71,8 +71,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       {accessToken ? (
-        <View>
+        <View style={styles.container}>
           <Text style={styles.texthome}>Bienvenido  </Text>
+          <Image 
+            style={styles.tinyLogohome}
+            source={img}
+          />
+          <Text style={{fontSize:15,color: "#FFFFFF", marginTop:30}}>
+          ¿QUIÉNES SOMOS? 
+          </Text>
+          <Text style={{fontSize:14, color: "#FFFFFF"}}>
+          Somos un equipo de expertos que ofrecen <Text style={{fontSize:14, color:"#E01747"}}> soluciones digitales</Text> que impactan.
+            
+          </Text>
         </View>
       ) : (
         <View style={styles.container}>
@@ -80,17 +91,22 @@ export default function App() {
             style={styles.tinyLogo}
             source={{uri:'https://canteradigital.mx/cantera2020/wp-content/uploads/2020/05/CanteraLogo.png'}}
           />
-
+  
           <Text style={styles.text}>Página de Registro</Text>
         </View>
       )}
       <View style={styles.containerbutton}>
+         
       <TouchableOpacity
         style={styles.button}
         onPress={accessToken ? getUserData : signInWithGoogleAsync}
       >
+           
         <Text style={styles.buttonText}>
-          
+        <Image
+              style={styles.logogmail}
+              source={icon}
+          /> 
           {accessToken ? "Detalles de cuenta" : "Iniciar con Google"}
         </Text>
       </TouchableOpacity>
@@ -114,16 +130,28 @@ const styles = StyleSheet.create({
     flex:0.5
   },
   tinyLogo:{
-    width:170,
-    height:50
+    width:185,
+    height:55
+    
   },
+  logogmail:{
+    
+    width:50,
+    height:30
+  },
+  tinyLogohome:{
+    width:170,
+    height:100,
+    borderRadius:80
+  },  
   text: {
     marginTop:20,
     fontSize: 28,
     color: "#FFFFFF"
   },
   texthome:{
-    fontSize:19,
+    marginLeft:20,
+    fontSize:20,
     color: "#FFFFFF"
   },  
   button: {
@@ -133,8 +161,8 @@ const styles = StyleSheet.create({
     padding: 15,
     borderBottomEndRadius: 4,
     borderBottomStartRadius: 4,
-    width: 190,
-    height: 55,
+    width: 230,
+    height: 70,
   },
   buttonText: {
     color: "#FFFFFF",
